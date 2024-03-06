12d970befd8ba7a13a666b98577c9d8926703190
Evandro Mattos <emattos0114@outlook.com>
2024-03-05 22:42:29 -0300

    Bad solution

--- model/entities/Account.java
+++ model/entities/Account.java
@@ -1,5 +1,44 @@
 package Bank.model.entities;
 
+import ExceptionSoluxan.model.exeception.DomainException;
+
 public class Account {
+
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withDrawLimit;
+
+    public Account(Integer number, String holder, Double balance, Double withDrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withDrawLimit = withDrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    public String getHolder() {
+        return holder;
+    }
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+    public Double getBalance() {
+        return balance;
+    }
+    public Double getWithDrawLimit() {
+        return withDrawLimit;
+    }
+    public void deposit(Double amount){
+        this.balance += amount;
+    }
+    public void withDraw(Double amount){
+        this.balance -= amount;
+    }
 
 }
